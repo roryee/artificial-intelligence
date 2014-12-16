0a4ac880eefea39ff6b85507ce620e7766019272
Ryan Duchene <duchenerc@gmail.com>
2014-12-16 15:27:28 -0600

    Add 404 template

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<main id="main" class="ww">
+	<article id="article">
+		<div class="c">
+			<article class="article cc c">
+				
+				<h1>404 Not Found</h1>
+				<p>
+					Whoops! That page doesn't seem to be here. Why don't you give the search engine a try?
+				</p>
+				<?php get_search_form(); ?>
+				
+			</article>
+		</div>
+	</article>
+</main>
+
+<?php get_footer(); ?>
