dc9d94b492522c55ca61d7511b99dc0a5e283761
Ryan Duchene <duchenerc@gmail.com>
2015-02-14 18:34:31 -0600

    Add solid block layout similar to Electrify's half-blocks

---
+++ block-solid.php
@@ -0,0 +1,15 @@
+<section class="solid">
+	<div class="ccc">
+		
+		<div class="c solid-thumbnail" style="background-image:url(<?php the_post_thumbnail_src( $post ); ?>);"></div>
+		<div class="c solid-headers">
+			<h1><?php the_field( 'heading' ); ?></h1>
+		</div>
+		<div class="c solid-content">
+			<p>
+				<?php the_field( 'content' ); ?>
+			</p>
+		</div>
+			
+	</div>
+</section>
